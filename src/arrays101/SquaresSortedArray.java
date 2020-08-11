d50f9ec884a5b7ea7aeeca124b6a6da99d0a6712
Chidozie Nnachor <odogwu.naija@gmail.com>
2020-08-12 00:29:03 +0200

    Initial commit

---
+++ src/arrays101/SquaresSortedArray.java
@@ -0,0 +1,19 @@
+package arrays101;
+
+import java.util.Arrays;
+
+public class SquaresSortedArray {
+
+    class Solution {
+        public int[] sortedSquares(int[] A) {
+
+            int[] newArray = new int[A.length];
+
+            for(int i = 0; i < A.length; i++) {
+                newArray[i] = A[i] * A[i];
+            }
+            Arrays.sort(newArray);
+            return newArray;
+        }
+    }
+}
